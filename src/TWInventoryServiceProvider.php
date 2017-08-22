1be1f0a5b9d3c1d187a628ff214a2fead9c49b4e
PJ Go <pjgo@tweeklabs.com>
2017-08-22 22:21:05 +0800

    first commit

---
+++ src/TWInventoryServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+namespace Sunnydevbox\TWInventory;
+
+use Illuminate\Support\ServiceProvider;
+
+class TWInventoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application events.
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+    }
+
+    protected function registerProviders()
+    {
+    }
+
+
+    protected function registerCommands()
+    {
+    }
+
+
+    protected function registerPublisher()
+    {
+
+    }
+}
